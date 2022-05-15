13a14d76f83b38fe139394041cfbf2d1439078b9
maievstorm <hungnp7686@gmail.com>
2022-05-15 16:38:08 +0700

    len

--- dags/dpa_spark_dag.py
+++ dags/dpa_spark_dag.py
@@ -11,22 +11,22 @@ args = {
 }
 
 with DAG(
-    dag_id='example_cassandra_etl',
+    dag_id='dpa_cassandra_etl',
     default_args=args,
     schedule_interval=None,
     start_date=days_ago(2),
-    tags=['example'],
+    tags=['hung'],
 ) as dag:
     # [START howto_operator_spark_submit]
-    os.environ['SPARK_HOME'] = '/workspace/example-cassandra-etl-with-airflow-and-spark/spark-3.0.1-bin-hadoop2.7'
-    sys.path.append(os.path.join(os.environ['SPARK_HOME'], 'bin'))
+    #os.environ['SPARK_HOME'] = '/workspace/example-cassandra-etl-with-airflow-and-spark/spark-3.0.1-bin-hadoop2.7'
+    #sys.path.append(os.path.join(os.environ['SPARK_HOME'], 'bin'))
 
     load_and_write = BashOperator(
         task_id="load_and_write_job",
         bash_command='spark-submit \
             --packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.1 \
             --properties-file /workspace/example-cassandra-etl-with-airflow-and-spark/properties.conf \
-            /workspace/example-cassandra-etl-with-airflow-and-spark/extract_and_load.py'
+            /dags_airflowdag/dags/00_script/py/extract_and_load.py'
     )
 
     etl_job = BashOperator(
@@ -34,7 +34,7 @@ with DAG(
         bash_command='spark-submit \
             --packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.1 \
             --properties-file /workspace/example-cassandra-etl-with-airflow-and-spark/properties.conf \
-            /workspace/example-cassandra-etl-with-airflow-and-spark/etl.py'
+            /dags_airflowdag/dags/00_script/py/etl.py'
     )
 
     load_and_write >> etl_job
